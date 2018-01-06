3e1abe0c2a9208d77ce79b25d2cd677eb2e678b3
MarcoXZh3 <zxu3@ualberta.ca>
2018-01-06 02:09:47 -0700

    Version1.2.1: findBlogs and findDelegations switched from steem to steemitsql

--- jobs/findBlogs.js
+++ jobs/findBlogs.js
@@ -1,10 +1,10 @@
 /**
  * The job to find blogs
  * @author:  MarcoXZh3
- * @version: 1.0.0
+ * @version: 1.1.0
  */
 var MongoClient = require('mongodb').MongoClient;
-var steem = require('steem');
+var sql = require("mssql");
 
 
 var moduleName = 'findBlogs';
@@ -16,6 +16,51 @@ var moduleName = 'findBlogs';
  *      @param {array}      newBlogs       the new blogs
  */
 module.exports = function(options, callback) {
+    var today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+    var query = (`SELECT * FROM Comments ` +                // the target table
+                 `WHERE depth = 0 ` +                       // only blogs
+                 `AND json_metadata LIKE '%"cn"%' ` +       // with "CN" tag
+                 `AND created >= 'TODAY'`)                  // posted today
+                .replace('TODAY', today.toISOString());
+
+    sql.connect(options.sqlserver, function(err) {
+        if (err) {
+            options.loggers[1].log('error',
+                                   '<' + moduleName + '.sql.connect> ' +
+                                   err.message);
+            if (callback) {
+                callback(err);
+            } // if (callback)
+            return err;
+        } // if (err)
+        new sql.Request().query(query, function(err, res) {
+            if (err) {
+                options.loggers[1].log('error',
+                                       '<' + moduleName + '.sql.Request.query> ' +
+                                       err.message);
+                if (callback) {
+                    callback(err);
+                } // if (callback)
+                return err;
+            } // if (err)
+
+            // Deal with these blog records
+            sql.close();
+            return FilterCnerBlogs(options, res.recordset, callback);
+        }); // new sql.Request().query(sql1, function(err, res) { ... });
+    }); // sql.connect(options.sqlserver, function(err) { ... });
+}; // module.exports = function(options, callback)
+
+
+/**
+ * Filter blogs only from the cners, taking blogs_per_day into consideration
+ * @param {json}        options     the options for the job
+ * @param {array}       allBlogs    the list of all today's blogs
+ * @param {function}    callback    the callback function
+ *      @param {array}      newBlogs       the new blogs, same as above
+ */
+var FilterCnerBlogs = function(options, allBlogs, callback) {
     MongoClient.connect(options.database, function(err, db) {
         if (err) {
             options.loggers[0].log('error',
@@ -37,157 +82,141 @@ module.exports = function(options, callback) {
                 return err;
             } // if (err)
 
-            var cners = res;
-            var today = new Date();
-            today.setUTCHours(0, 0, 0, 0);
-            db.collection('blogs').find({ created:{ $gte:today } }).toArray(function(err, res) {
-                if (err) {
-                    options.loggers[1].log('error',
-                                           '<' + moduleName + '.db.blogs.find> ' +
-                                           err.message);
-                    if (callback) {
-                        callback(err);
-                    } // if (callback)
-                    return err;
-                } // if (err)
+            // Filter out the blogs
+            var blogMap = {};
+            allBlogs.forEach(function(b) {
+                var idx = res.findIndex( (e)=>e.name===b.author );
+                if (idx < 0) {
+                    return ;
+                } // if (idx < 0)
+                var upvoteat = new Date(b.created.getTime() + 1000 * options.time_upvote);
+                var now = new Date();
+                if (upvoteat.getTime() < now.getTime() + options.delay) {
+                    upvoteat = new Date(now.getTime() + 2 * options.delay);
+                } // if (upvoteat.getTime() < now.getTime() + options.delay)
+                var blog = {
+                    scheduled:  false,
+                    upvoted:    false,
+                    replied:    false,
+                    created:    b.created,
+                    upvoteat:   upvoteat,
+                    author:     b.author,
+                    vests:      res[idx].vests,
+                    membertime: res[idx].membertime,
+                    permlink:   b.permlink,
+                    title:      b.title
+                }; // var blog = { ... };
+                if (b.author in blogMap) {
+                    if (blogMap[b.author].length < options.blogs_per_day) {
+                        blogMap[b.author].push(blog);
+                    } // if (blogMap[b.author].length < options.blogs_per_day)
+                } else {
+                    blogMap[b.author] = [blog];
+                } // else - if (b.author in blogMap)
+            }); // allBlogs.forEach(function(b) {});
+            var targetBlogs = [];
+            Object.keys(blogMap).forEach(function(k) {
+                targetBlogs = targetBlogs.concat(blogMap[k]);
+            }); // Object.keys(blogMap).forEach(function(k) { ... });
 
-                // Find blogs for each of the cners
-                FindAuthorBlogs(options, db, cners, 0, res, [], function(newBlogs) {
-                    if (callback) {
-                        callback(newBlogs);
-                    } // if (callback)
-                    return newBlogs;
-                });// FindAuthorBlogs(options, db, cners, 0, res, [], function(newBlogs) { ... });
-
-            }); // db.collection('blogs').find({ created:{ $gte:today } }).toArray();
+            // Go next
+            return GoupBlogs(options, db, targetBlogs, callback);
         }); // db.collection('cners').find({}).toArray( ... );
     }); // MongoClient.connect(options.database, function(err, db) { ... });
-}; // module.exports = function(options, callback)
+}; // var FilterCnerBlogs = function(options, allBlogs, callback) { ... };
+
 
 /**
- * Find blogs for each of the cners as authors
+ * Group blogs into new and old
  * @param {json}        options     the options for the job
  * @param {object}      db          the database handle
- * @param {array}       cners       the list of all cners
- * @param {integer}     idx         the index of the current cner
- * @param {array}       todayBlogs  the list of today's blogs (to be upvoted only)
- * @param {array}       newBlogs    the list of newly detected blogs
+ * @param {array}       targetBlogs the list of all target blogs
  * @param {function}    callback    the callback function
  *      @param {array}      newBlogs       the new blogs, same as above
  */
-var FindAuthorBlogs = function(options, db, cners, idx, todayBlogs, newBlogs, callback) {
-    var author = cners[idx].name;
-    steem.api.getBlog(author, options.huge_number, 500, function(err, res) {
+var GoupBlogs = function(options, db, targetBlogs, callback) {
+    var today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+    db.collection('blogs').find({ created:{ $gte:today } }).toArray(function(err, res) {
         if (err) {
             options.loggers[1].log('error',
-                                   '<' + moduleName + '.steem.api.getBlog> ' +
+                                   '<' + moduleName + '.db.blogs.find> ' +
                                    err.message);
-            if (idx === cners.length - 1) {
-                if (callback) {
-                    callback(err);
-                } // if (callback)
-                return err;
-            } else {
-                FindAuthorBlogs(options, db, cners, idx+1, todayBlogs, newBlogs, callback);
-            } // if (idx === cners.length - 1)
+            if (callback) {
+                callback(err);
+            } // if (callback)
+            return err;
         } // if (err)
 
-        var today = new Date();
-        today.setUTCHours(0, 0, 0, 0);
-        var authorTodayBlogs = res.map(function(e) {
-            var blog = e.comment;
-            if (!blog.created.endsWith('Z')) {
-                blog.created = new Date(blog.created + '.000Z');
-            } // if (!blog.created.endsWith('Z'))
-            if (blog.created.getTime() < today.getTime()) {
-                return null;                    // do not consider previous posts
-            } // if (blog.created < today)
-            if (blog.author !== author) {       // do not consider reblogged posts
-                return null;
-            } // if (blog.author !== author)
-            var rawTags = JSON.parse(blog.json_metadata).tags;
-            if (rawTags.filter( (e)=>e.toLowerCase()==='cn' ).length === 0) {
-                return null;                    // do not consider non-cn posts
-            } // if ( ... )
-            var upvoteat = new Date(blog.created.getTime() + 1000 * options.time_upvote);
-            var now = new Date();
-            if (upvoteat.getTime() < now.getTime() + options.delay) {
-                upvoteat = new Date(now.getTime() + 2 * options.delay);
-            } // if (upvoteat.getTime() < now.getTime() + options.delay)
-            return {
-                scheduled:  false,
-                upvoted:    false,
-                replied:    false,
-                created:    blog.created,
-                upvoteat:   upvoteat,
-                author:     blog.author,
-                vests:      cners[idx].vests,
-                membertime: cners[idx].membertime,
-                permlink:   blog.permlink,
-                title:      blog.title
-            }; // return { ... };
-        }).filter( (e)=>e )                                         // today's blogs
-          .sort( (a,b)=>a.created.getTime()-b.created.getTime() )   // sort asendently
-          .splice(0, options.blogs_per_day);                        // first several blogs
+        // Group here
+        var newBlogs = [];
+        var oldBlogs = [];
+        targetBlogs.sort( (a,b)=>a.created.getTime()-b.created.getTime())
+                   .forEach(function(b) {
+            var idx = res.findIndex( (e)=>e.author===b.author&&
+                                          e.permlink===b.permlink );
+            if (idx < 0) {      // New blogs are not in records
+                newBlogs.push(b);
+            } else {            // Old blogs are in, but not voted
+                if (!res[idx].upvoted) {
+                    oldBlogs.push(b);
+                } // if (!res[idx].upvoted)
+            } // if (idx < 0)
+        }); // targetBlogs.sort( ... ).forEach( ... );
 
-        // New blogs: today's blogs that are not NOT IN blogs (thus not voted)
-        var newAuthorBlogs = authorTodayBlogs.filter(function(e) {
-            return 0 > todayBlogs.findIndex( (b)=>b.author===e.author&&
-                                                  b.permlink===e.permlink );
-        }); // var newAuthorBlogs = authorTodayBlogs.filter(function(e) { ... });
-        newBlogs = newBlogs.concat(newAuthorBlogs);
+        // Go next
+        return saveBlogsTodb(options, db, newBlogs, oldBlogs, callback);
+    }); // db.collection('blogs').find({ created:{ $gte:today } }).toArray();
+}; // var GoupBlogs = function(options, db, targetBlogs, callback) { ... };
 
-        // Log and save new blogs
-        if (newAuthorBlogs.length === 0) {
-            options.loggers[1].log('info',
-                                   '<' + moduleName + '> No new blogs found (' +
-                                   (idx+1) + '/' + cners.length + '): ' +
-                                   cners[idx].name);
-        } else {
-            options.loggers[1].log('info',
-                                   '<' + moduleName + '> New blogs found (' +
-                                   (idx+1) + '/' + cners.length + '): ' +
-                                   cners[idx].name + ' - ' + newAuthorBlogs.length);
-            db.collection('blogs').insertMany(newAuthorBlogs, function(err, res) {
+
+/**
+ * Save the blog into database
+ * @param {json}        options     the options for the job
+ * @param {object}      db          the database handle
+ * @param {array}       newBlogs    the list of new blogs
+ * @param {array}       oldBlogs    the list of old blogs
+ * @param {function}    callback    the callback function
+ *      @param {array}      newBlogs       the new blogs, same as above
+ */
+var saveBlogsTodb = function(options, db, newBlogs, oldBlogs, callback) {
+    // Nnew blogs -- insert
+    if (newBlogs.length === 0) {
+        options.loggers[1].log('info',
+                                '<' + moduleName + '> No new blogs found');
+    } else {
+        options.loggers[1].log('info',
+                                '<' + moduleName + '> New blogs found: '
+                                    + newBlogs.length);
+        db.collection('blogs').insertMany(newBlogs, function(err, res) {
+            if (err) {
+                options.loggers[1].log('error',
+                                        '<' + moduleName + '.db.blogs.insertMany> ' +
+                                        err.message);
+            } // if (err)
+        }); // db.collection('blogs').insertMany( ... );
+    } // else - if (newBlogs.length === 0)
+
+    // Old blogs -- update upvoteat if necessary
+    oldBlogs.forEach(function(b) {
+        var now = new Date();
+        if (b.upvoteat.getTime() < now.getTime() + options.delay) {
+            b.upvoteat = new Date(now.getTime() + 2 * options.delay);
+            db.collection('blogs').update({ author:b.author, permlink:b.permlink },
+                                          { $set:{ upvoteat:b.upvoteat } },
+                                          function(err, res) {
                 if (err) {
                     options.loggers[1].log('error',
-                                           '<' + moduleName + '.db.blogs.insertMany> ' +
+                                           '<' + moduleName + '.db.blogs.update> ' +
                                            err.message);
                 } // if (err)
-            }); // db.collection('blogs').insertMany( ... );
-        } // else - if (newAuthorBlogs.length === 0)
+            }); // db.collection('blogs').update( ... );
+        } // if (b.upvoteat.getTime() < now.getTime() + options.delay)
+    }); // oldBlogs.forEach(function(b) { ... });
 
-        // Old blogs: today's blogs that are IN blogs but not voted
-        var oldAuthorBlogs = authorTodayBlogs.forEach(function(e) {
-            var blog = todayBlogs.filter( (b)=>!b.upvoted&&b.author===e.author
-                                                         &&b.permlink===e.permlink )[0];
-            if (!blog) {
-                return ;
-            } // if (!blog)
-            var now = new Date();
-            if (blog.upvoteat.getTime() < now.getTime() + options.delay) {
-                blog.upvoteat = new Date(now.getTime() + 2 * options.delay);
-                db.collection('blogs').update({ author:blog.author, permlink:blog.permlink },
-                                              { $set:{ upvoteat:blog.upvoteat } },
-                                              function(err, res) {
-                    if (err) {
-                        options.loggers[1].log('error',
-                                               '<' + moduleName + '.db.blogs.update> ' +
-                                               err.message);
-                    } // if (err)
-                }); // db.collection('blogs').update( ... );
-            } // if (blog.upvoteat.getTime() < now.getTime() + options.delay)
-        }); // var oldAuthorBlogs = authorTodayBlogs.forEach(function(e) { ... });
-
-        // next loop, if any
-        if (idx === cners.length - 1) {
-            if (callback) {
-                callback(newBlogs);
-            } // if (callback)
-            return newBlogs;
-        } else {
-            FindAuthorBlogs(options, db, cners, idx+1, todayBlogs, newBlogs, callback);
-        } // else - if (idx === cners.length - 1)
-
-    }); // steem.api.getBlog(author, options.huge_number, 500, function(err, res) {
-}; // var FindAuthorBlogs = function(options, db, cners, idx, todayBlogs, newBlogs, callback) { ... };
+    // All done
+    if (callback) {
+        callback(newBlogs);
+    } // if (callback)
+    return newBlogs;
+}; // var saveBlogsTodb = function(options, db, newBlogs, oldBlogs, callback)
